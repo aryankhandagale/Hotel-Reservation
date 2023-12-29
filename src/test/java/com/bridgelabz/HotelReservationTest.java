117953ff2cd0da88ca4b0b8ce84219aba1fccd4c
aryankhandagale <152610482+aryankhandagale@users.noreply.github.com>
2023-12-29 15:18:47 +0530

    Refactored UC2

--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -5,13 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HotelReservationTest {
     @Test
-    void testCalculateTotalCost() {
+    void testCalculateLakewoodCost() {
         HotelReservation hotelReservation = new HotelReservation();
         String startDate = "10/9/2020";
         String endDate = "11/9/2020";
 
         int lakewoodCost = hotelReservation.calculateTotalCost(startDate, endDate, hotelReservation.lakewood);
-
         assertEquals(220, lakewoodCost);
+    }
+
+    @Test
+    void testCalculateBridgewoodCost() {
+        HotelReservation hotelReservation = new HotelReservation();
+        String startDate = "10/9/2020";
+        String endDate = "11/9/2020";
+
+        int bridgewoodCost = hotelReservation.calculateTotalCost(startDate, endDate, hotelReservation.bridgewood);
+        assertEquals(320, bridgewoodCost);
+    }
+
+    @Test
+    void testCalculateRidgewoodCost() {
+        HotelReservation hotelReservation = new HotelReservation();
+        String startDate = "10/9/2020";
+        String endDate = "11/9/2020";
+
+        int ridgewoodCost = hotelReservation.calculateTotalCost(startDate, endDate, hotelReservation.ridgewood);
+        assertEquals(440, ridgewoodCost);
     }
 }
