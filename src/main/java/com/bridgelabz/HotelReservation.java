5604cf7b3c5723efea9abf8bd5e52a5530b00928
aryankhandagale <aryan130601@gmail.com>
2023-12-29 12:29:12 +0530

    Added: UC2 Find Cheapest Hotel in date range

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,5 +1,11 @@
 package com.bridgelabz;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 class HotelDetails {
     private String hotelName;
     private int weekDayRate;
@@ -15,27 +21,62 @@ class HotelDetails {
     public int getWeekDayRate() {
         return weekDayRate;
     }
-
-    public void printHotelDetails() {
-        System.out.println("Hotel Name: " + hotelName);
-        System.out.println("Weekday Rate: $" + weekDayRate);
-    }
 }
 
 public class HotelReservation {
-    private static int hotelCount = 0;
     HotelDetails lakewood = new HotelDetails("Lakewood", 110);
 
-    public HotelReservation() {
-        hotelCount++;
+    public int calculateTotalCost(String startDateString, String endDateString, HotelDetails hotel) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date startDate = dateFormat.parse(startDateString);
+            Date endDate = dateFormat.parse(endDateString);
+
+            Calendar startCalendar = Calendar.getInstance();
+            startCalendar.setTime(startDate);
+
+            Calendar endCalendar = Calendar.getInstance();
+            endCalendar.setTime(endDate);
+
+            int totalCost = 0;
+
+            while (startCalendar.before(endCalendar) || startCalendar.equals(endCalendar)) {
+                int rate = hotel.getWeekDayRate();
+                totalCost += rate;
+
+                startCalendar.add(Calendar.DAY_OF_MONTH, 1);
+            }
+            return totalCost;
+        } catch (ParseException e) {
+            System.out.println("Invalid date format. Please enter dates in DD/MM/YYYY format");
+            return -1;
+        }
     }
 
-    public static int getHotelCount() {
-        return hotelCount;
+    public HotelDetails findCheapest(String startDate, String endDate) {
+        int lakewoodCost = calculateTotalCost(startDate, endDate, lakewood);
+        return lakewood;
     }
+}
 
+class Booking {
     public static void main(String[] args) {
-        HotelReservation reservation = new HotelReservation();
-        reservation.lakewood.printHotelDetails();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the check-in date (DD/MM/YYYY): ");
+        String startDateString = scanner.nextLine();
+
+        System.out.print("Enter the check-out date (DD/MM/YYYY): ");
+        String endDateString = scanner.nextLine();
+
+        HotelReservation hotelReservation = new HotelReservation();
+        HotelDetails cheapestHotel = hotelReservation.findCheapest(startDateString, endDateString);
+
+        if (cheapestHotel != null) {
+            System.out.println("Cheapest Hotel: " + cheapestHotel.getHotelName());
+            System.out.println("Total Cost: " + hotelReservation.calculateTotalCost
+                    (startDateString, endDateString, cheapestHotel));
+        }
+        scanner.close();
     }
 }
--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HotelReservationTest {
     @Test
-    void testHotelCount() {
-        HotelReservation lakewood = new HotelReservation();
+    void testCalculateTotalCost() {
+        HotelReservation hotelReservation = new HotelReservation();
+        String startDate = "10/9/2020";
+        String endDate = "11/9/2020";
 
-        assertEquals(1, HotelReservation.getHotelCount());
+        int lakewoodCost = hotelReservation.calculateTotalCost(startDate, endDate, hotelReservation.lakewood);
+
+        assertEquals(220, lakewoodCost);
     }
 }
