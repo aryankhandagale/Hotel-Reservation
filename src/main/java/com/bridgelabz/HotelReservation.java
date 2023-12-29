688ed003e92de62d39b3ef892d9316a643e9b938
aryankhandagale <aryan130601@gmail.com>
2023-12-29 12:24:54 +0530

    Added: UC1 Ability to add a hotel with name and rate

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,7 +1,41 @@
 package com.bridgelabz;
 
+class HotelDetails {
+    private String hotelName;
+    private int weekDayRate;
+
+    public HotelDetails (String hotelName, int weekDayRate) {
+        this.hotelName = hotelName;
+        this.weekDayRate = weekDayRate;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+    public int getWeekDayRate() {
+        return weekDayRate;
+    }
+
+    public void printHotelDetails() {
+        System.out.println("Hotel Name: " + hotelName);
+        System.out.println("Weekday Rate: $" + weekDayRate);
+    }
+}
+
 public class HotelReservation {
+    private static int hotelCount = 0;
+    HotelDetails lakewood = new HotelDetails("Lakewood", 110);
+
+    public HotelReservation() {
+        hotelCount++;
+    }
+
+    public static int getHotelCount() {
+        return hotelCount;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation Program");
+        HotelReservation reservation = new HotelReservation();
+        reservation.lakewood.printHotelDetails();
     }
 }
--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -4,5 +4,10 @@ import  org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HotelReservationTest {
+    @Test
+    void testHotelCount() {
+        HotelReservation lakewood = new HotelReservation();
 
+        assertEquals(1, HotelReservation.getHotelCount());
+    }
 }
